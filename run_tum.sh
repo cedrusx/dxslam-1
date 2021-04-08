0e035e9e358b8b0ebf86d633a0451a00facec313
Xuesong Shi <xuesong.shi@intel.com>
2021-04-08 20:01:01 +0800

    recognize association.txt and argument of yaml file
    Signed-off-by: Xuesong Shi <xuesong.shi@intel.com>

--- run_tum.sh
+++ run_tum.sh
@@ -2,20 +2,31 @@
 
 FOLDER=$1
 
-if [[ "$FOLDER" == *"freiburg1"* ]]; then
-    CONFIG=TUM1.yaml
+if [[ "$2" != "" ]]; then
+    CONFIG=$2
+elif [[ "$FOLDER" == *"freiburg1"* ]]; then
+    CONFIG=Examples/RGB-D/TUM1.yaml
 elif [[ "$FOLDER" == *"freiburg2"* ]]; then
-    CONFIG=TUM2.yaml
+    CONFIG=Examples/RGB-D/TUM2.yaml
 elif [[ "$FOLDER" == *"freiburg3"* ]]; then
-    CONFIG=TUM3.yaml
+    CONFIG=Examples/RGB-D/TUM3.yaml
 else
     echo "Usage: $0 DATA_FOLDER CONFIG_FILE"
     exit
 fi
 
-echo ./Examples/RGB-D/rgbd_tum Vocabulary/DXSLAM.fbow Examples/RGB-D/$CONFIG $1 $1/associations.txt $1/features_hfnet
+if test -f "$FOLDER/associations.txt"; then
+    ASSOCIATION=$FOLDER/associations.txt
+elif test -f "$FOLDER/association.txt"; then
+    ASSOCIATION=$FOLDER/association.txt
+else
+    echo "Association file not found under $1"
+    exit
+fi
 
-./Examples/RGB-D/rgbd_tum Vocabulary/DXSLAM.fbow Examples/RGB-D/$CONFIG $1 $1/associations.txt $1/features_hfnet
+echo ./Examples/RGB-D/rgbd_tum Vocabulary/DXSLAM.fbow $CONFIG $FOLDER $ASSOCIATION $FOLDER/features_hfnet
+
+./Examples/RGB-D/rgbd_tum Vocabulary/DXSLAM.fbow $CONFIG $FOLDER $ASSOCIATION $FOLDER/features_hfnet
 cp CameraTrajectory.txt $1/trajectory-dxslam.txt
 evo_ape tum $1/groundtruth.txt $1/trajectory-dxslam.txt -a -p
 wc CameraTrajectory.txt
